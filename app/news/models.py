4b612a1fd468f1bb5f4d407c8a907109e048195a
Sudhanva Narayana <nsudhanva@gmail.com>
2022-12-05 07:04:07 -0500

    added news label and other things

--- app/news/models.py
+++ app/news/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 
-# Create your models here.
 class NewsArticle(models.Model):
     link = models.URLField()
     headline = models.CharField(max_length=1000)
@@ -8,3 +7,17 @@ class NewsArticle(models.Model):
     short_description = models.TextField()
     authors = models.CharField(max_length=100)
     date = models.DateTimeField()
+
+class NewsLabel(models.Model):
+    id = models.AutoField(primary_key=True)
+    value = models.IntegerField()
+    title = models.CharField(max_length=255)
+
+class NewsArticleNewsLabel(models.Model):
+    news_article = models.ForeignKey('NewsArticle', on_delete=models.CASCADE)
+    news_label = models.ForeignKey('NewsLabel', on_delete=models.CASCADE)
+    percentage = models.FloatField()
+
+class NewsVote(models.Model):
+    news_article = models.ForeignKey('NewsArticle', on_delete=models.CASCADE)
+    news_label = models.ForeignKey('NewsLabel', on_delete=models.CASCADE)
--- app/news/views.py
+++ app/news/views.py
@@ -1,11 +1,35 @@
 from django.http import HttpResponse
 from django.template import loader
-from .models import NewsArticle
+from .models import NewsVote, NewsArticle, NewsLabel, NewsArticleNewsLabel
 
 def index(request):
-    articles = NewsArticle.objects.all()
     template = loader.get_template('index.html')
+    articles = NewsArticle.objects.all()
+    news_votes = NewsVote.objects.all()
+        
     context = {
         'articles': articles,
+        'votes': news_votes,
     }
-    return HttpResponse(template.render(context, request))
+
+    if request.method == 'GET':
+        
+        for i in news_votes:
+            print(i.news_label.title)
+
+        return HttpResponse(template.render(context, request))
+    elif request.method == 'POST':
+
+        option = request.POST.get('option')
+        article_id = request.POST.get('article_id')
+
+        # Get the NewsArticle and NewsLabel objects
+        article = NewsArticle.objects.get(id=article_id)
+        label = NewsLabel.objects.get(id=option)
+
+        # Create and save the NewsVote object
+        vote = NewsVote(news_article=article, news_label=label)
+        vote.save()
+
+        # Return an HttpResponse with the payload's content
+        return HttpResponse(template.render(context, request))
